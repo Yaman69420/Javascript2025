5fff08e47ae2f6843728f5c2ef3fb76f34c8577e
Yaman Terkawi <Yaman.terkawi.yt@gmail.com>
2025-09-24 12:32:30 +0200

    4

--- 2Oef/Oef10/js/index.js
+++ 2Oef/Oef10/js/index.js
@@ -1,5 +1,4 @@
-//manlsd
-let getal = prompt("Geef een getal in:");
-for (let i = 1; i <= 10; i++) {
-    console.log(`${getal} x ${i} = ${getal * i}`);
+let getal1 = prompt("geef een getal in");
+for (let i = 1; i <= 10; i=i+1){
+    console.log(`${getal1} x ${i} = ${getal1 * i}`);
 }
